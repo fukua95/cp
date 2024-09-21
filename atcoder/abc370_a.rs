f83b4ab49dcaa3abf2c87e597e5ff859ac124ef9
fukua95 <fukua95@gmail.com>
2024-09-21 20:33:58 +0800

    test my Rust IO module

---
+++ atcoder/abc370_a.rs
@@ -0,0 +1,206 @@
+use std::fmt::Debug;
+use std::io::Read;
+use std::str::FromStr;
+
+pub struct Input {
+    input: Box<dyn Read>,
+    buf: Vec<u8>,
+    at: usize,
+    buf_read: usize,
+}
+
+impl Input {
+    const DEFAULT_BUF_SIZE: usize = 4096;
+
+    pub fn new() -> Self {
+        Self {
+            input: Box::new(std::io::stdin()),
+            buf: vec![0; Self::DEFAULT_BUF_SIZE],
+            at: 0,
+            buf_read: 0,
+        }
+    }
+
+    pub fn get(&mut self) -> Option<u8> {
+        if self.refill_buffer() {
+            let res = self.buf[self.at];
+            self.at += 1;
+            Some(res)
+        } else {
+            None
+        }
+    }
+
+    pub fn peek(&mut self) -> Option<u8> {
+        if self.refill_buffer() {
+            Some(self.buf[self.at])
+        } else {
+            None
+        }
+    }
+
+    pub fn skip_whitespace(&mut self) {
+        while let Some(b) = self.peek() {
+            if !char::from(b).is_whitespace() {
+                return;
+            }
+            self.get();
+        }
+    }
+
+    pub fn next_token(&mut self) -> Option<Vec<u8>> {
+        self.skip_whitespace();
+        let mut res = Vec::new();
+        while let Some(c) = self.get() {
+            if char::from(c).is_whitespace() {
+                break;
+            }
+            res.push(c);
+        }
+        if res.is_empty() {
+            None
+        } else {
+            Some(res)
+        }
+    }
+
+    pub fn read<T: Readable>(&mut self) -> T {
+        T::read(self)
+    }
+
+    pub fn vec<T: Readable>(&mut self, size: usize) -> Vec<T> {
+        let mut res = Vec::with_capacity(size);
+        for _ in 0usize..size {
+            res.push(self.read());
+        }
+        res
+    }
+
+    pub fn string_vec(&mut self, size: usize) -> Vec<Vec<u8>> {
+        let mut res = Vec::with_capacity(size);
+        for _ in 0usize..size {
+            res.push(self.string());
+        }
+        res
+    }
+
+    pub fn read_line(&mut self) -> String {
+        let mut res = String::new();
+        while let Some(c) = self.get() {
+            if c == b'\n' {
+                break;
+            }
+            if c == b'\r' {
+                if self.peek() == Some(b'\n') {
+                    self.get();
+                }
+                break;
+            }
+            res.push(c.into());
+        }
+        res
+    }
+
+    fn read_integer<T: FromStr>(&mut self) -> T
+    where
+        <T as FromStr>::Err: Debug,
+    {
+        self.read_string().parse::<T>().unwrap()
+    }
+
+    fn read_string(&mut self) -> String {
+        match self.next_token() {
+            None => {
+                panic!("Input exhausted");
+            }
+            Some(res) => unsafe { String::from_utf8_unchecked(res) },
+        }
+    }
+
+    pub fn string(&mut self) -> Vec<u8> {
+        self.read_string().into_bytes()
+    }
+
+    fn read_char(&mut self) -> char {
+        self.skip_whitespace();
+        self.get().unwrap().into()
+    }
+
+    fn refill_buffer(&mut self) -> bool {
+        if self.at == self.buf_read {
+            self.at = 0;
+            self.buf_read = self.input.read(&mut self.buf).unwrap();
+            self.buf_read != 0
+        } else {
+            true
+        }
+    }
+}
+
+pub trait Readable {
+    fn read(input: &mut Input) -> Self;
+}
+
+impl Readable for String {
+    fn read(input: &mut Input) -> Self {
+        input.read_string()
+    }
+}
+
+impl Readable for char {
+    fn read(input: &mut Input) -> Self {
+        input.read_char()
+    }
+}
+
+impl Readable for f64 {
+    fn read(input: &mut Input) -> Self {
+        input.read_string().parse().unwrap()
+    }
+}
+
+impl<T: Readable> Readable for Vec<T> {
+    fn read(input: &mut Input) -> Self {
+        let size = input.read();
+        input.vec(size)
+    }
+}
+
+macro_rules! read_integer {
+    ($t:ident) => {
+        impl Readable for $t {
+            fn read(input: &mut Input) -> Self {
+                input.read_integer()
+            }
+        }
+    };
+}
+
+read_integer!(i8);
+read_integer!(i16);
+read_integer!(i32);
+read_integer!(i64);
+read_integer!(i128);
+read_integer!(isize);
+read_integer!(u8);
+read_integer!(u16);
+read_integer!(u32);
+read_integer!(u64);
+read_integer!(u128);
+read_integer!(usize);
+
+fn main() {
+    let mut input = Input::new();
+    // let l: i32 = input.read_integer();
+    // let r: i32 = input.read_integer();
+    let l = i32::read(&mut input);
+    let r = i32::read(&mut input);
+
+    let res = match (l, r) {
+        (1, 0) => "Yes",
+        (0, 1) => "No",
+        _ => "Invalid",
+    };
+
+    println!("{}", res);
+}
